98187bfc6403bea8ef25e7f9856ebcd8368fea96
Jack <jah257@exeter.ac.uk>
2016-10-03 12:01:13 +0100

    added doc comments to MyTask

--- Job.java
+++ Job.java
@@ -4,17 +4,17 @@ public class Job implements Runnable {
     Job (int jobNumber) {
         this.jobNumber = jobNumber;
     }
-    
+    //Testing the commenting
     public void run () {
     // Undertake required work, here we will emulate it by sleeping for a period 
-     System.out.println("Job: " + jobNumber + " is being processed by thread : " + 
-        + Thread.currentThread ().getName() );
+     System.out.println("Job: " + jobNumber + " is being processed by thread : " 
+     + Thread.currentThread ().getName() );
     try {
         Thread.sleep((int)(1000));
     } catch (InterruptedException e) {
         // no catching as exmaple should not experience interruptions
     }
-    
+    //Hello
     System.out.println("Job: " + jobNumber + " is ending in thread : " + Thread.currentThread().getName() );
   }
   
